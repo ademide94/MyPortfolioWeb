4807b31758a11c2771872635364649d8a6a1399f
ibrahim ademide <49987039+ademide94@users.noreply.github.com>
2022-09-21 20:08:55 +0100

    update

--- MyScript.js
+++ MyScript.js
@@ -1,1 +1,18 @@
-// JavaScript source code
+ 
+
+jQuery(document).ready(function () {
+    setTimeout(function () { 
+        $("#first").css("visibility", "visible"); 
+    }, 520);
+
+    setTimeout(function () {
+        $("#second").css("visibility", "visible");
+    }, 1050);
+});
+
+
+function HomeFunction() { 
+    $("#Home_Div").css("display", "hidden"); 
+    return p1 * p2;   
+}
+ 
